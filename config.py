f57526ea528256f475c552edf12e3f4e741de0bb
Samuel Cacela <scacela>
2022-08-03 12:20:19 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,28 @@
+# authentication method: "api_key" or "resource_principal"
+auth_method="resource_principal"
+# config file path
+config_file_path="~/.oci/config"
+# config file profile
+config_profile="DEFAULT"
+# compartment id of create_image_job
+compartment_id = "<COMPARTMENT OCID>"
+# model id
+model_id = "<AI VISION MODEL OCID>"
+
+# namespace of Object Storage location containing the images to be analyzed
+object_location_namespace_name="<OBJECT STORAGE NAMESPACE FOR INFERENCE OBJECTS>"
+# Object Storage bucket containing the images to be analyzed
+object_location_bucket_name="BUCKET FOR INFERENCE OBJECTS"
+# Prefix in bucket containing images to be analyzed
+object_location_prefix=""
+
+# namespace of Object Storage location where the image analysis will be generated
+output_location_namespace_name="<OBJECT STORAGE NAMESPACE FOR OUTPUT ANALYSIS>"
+# Object Storage bucket where the image analysis will be generated
+output_location_bucket_name="BUCKET FOR INFERENCE OBJECTS"
+# Prefix in bucket where image analysis will be generated
+output_location_prefix="Analysis/"
+
+# Generate a zip file for all analyzed results in user-specified output location in object storage
+# set is_zip_output_enabled true to enable zip output feature
+is_zip_output_enabled=True
---
+++ list_objects.py
@@ -0,0 +1,35 @@
+import time
+import oci
+import json
+from config import *
+
+def main():
+    if auth_method == "resource_principal":
+        rps = oci.auth.signers.get_resource_principals_signer()
+        ai_vision_client = oci.object_storage.ObjectStorageClient(config={}, signer=rps)
+    elif auth_method == "api_key":
+        config = oci.config.from_file(config_file_path, config_profile)
+        ai_vision_client = oci.object_storage.ObjectStorageClient(config=config, retry_strategy=retry_strategy)
+    else:
+        print("Assign a value to auth_method in config.py: \"resource_princpal\" or \"api_key\".")
+
+    # API call
+    res=object_storage_client.list_objects(
+        namespace_name=object_location_namespace_name,
+        bucket_name=object_location_bucket_name,
+        prefix=object_location_prefix,
+        start=None,
+        end=None,
+        limit=None,
+        delimiter=None,
+        fields=None,
+        opc_client_request_id=None,
+        start_after=None)
+
+    object_array=[]
+    for i in res.data.objects:
+        # exclude objects that are folders
+        if i.name[-1]!="/":
+            object_array.append(i.name)
+
+    return object_array
